3dac2cf741f4952a74f1721af34bc9956f97773a
unknown <vane0121@gmail.com>
2026-08-02 17:31:50 +0800

    change teh http with https

--- js/main.js
+++ js/main.js
@@ -418,7 +418,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     try {
       // const response = await fetch("https://localhost:7284/api/chat", {
-      const response = await fetch("http://vanessabattung-api.runasp.net/api/Chat", {
+      const response = await fetch("https://vanessabattung-api.runasp.net/api/Chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: question })
